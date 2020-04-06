0ec3b39683779a1920c89deff9e3119928fbb468
Chantelsky <chantel.sky@outlook.com>
2020-04-06 21:51:31 +1000

    formatting

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <nav className="navbar bg-dark">
-        <h1>
-          <Link to="/">DevConnector</Link>
-        </h1>
-        <ul>
-          <li><Link to="!#">Developers</Link></li>
-          <li><Link to="/register">Register</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
-      </nav>
-    )
-}
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>DevConnector</Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to='!#'>Developers</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,19 +1,19 @@
-import { SET_ALERT, REMOVE_ALERT } from '../actions/types'
-const initialState = []
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+const initialState = [];
 
 //payload = data
 export default function (state = initialState, action) {
-    const { type, payload } = action
+  const { type, payload } = action;
 
-    switch (type) {
-        //dispatch this type
-        case SET_ALERT:
-            //return the array with payload
-            return [...state, payload]
-        case REMOVE_ALERT:
-            //filters through and return all alerts except for one that matches the payload
-            return state.filter(alert => alert.id !== payload)
-        default:
-            return state
-    }
+  switch (type) {
+    //dispatch this type
+    case SET_ALERT:
+      //return the array with payload
+      return [...state, payload];
+    case REMOVE_ALERT:
+      //filters through and return all alerts except for one that matches the payload
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
 }

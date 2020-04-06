dc5950fd3d7e648f4f4268ebed9a7217416130ab
Chantelsky <chantel.sky@outlook.com>
2020-04-06 21:29:36 +1000

    Alert reducer, action types with redux

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -7,38 +7,38 @@ const Login = () => {
         password: ''
     });
 
-    const {  email, password } = formData;
+    const { email, password } = formData;
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
     const onSubmit = async e => {
-        e.preventDefault();
-        console.log('woohoo!');
-    };
+        e.preventDefault()
+        console.log('woohoo!')
+    }
 
     return (
-    <Fragment>
-    <h1 className="large text-primary">Sign In</h1>
-        <p className="lead">Sign into your account</p>
-        <form className="form" onSubmit={e => onSubmit(e)}>
-            <div className="form-group">
-                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
-            </div>
-            <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={password}
-                    onChange={e => onChange(e)}
-                    minLength="6"/>
-            </div>
-            <input type="submit" className="btn btn-primary" value="Login" />
-        </form>
-        <p className="my-1">
-            Don't have an account <Link to ="/register">Sign In</Link>
-        </p>
-    </Fragment>
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead">Sign into your account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength="6" />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account <Link to="/register">Sign In</Link>
+            </p>
+        </Fragment>
     )
 }
 
